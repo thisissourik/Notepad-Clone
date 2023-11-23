efd2309d3ab998cce9c57f6f8cac27e3875fdd10
Sourik Karmakar <116931112+thisissourik@users.noreply.github.com>
2023-11-23 11:38:18 +0530

    Add files via upload

---
+++ Notepad.java
@@ -0,0 +1,203 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.print.PrinterException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+// import java.util.EventListener;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class Notepad extends JFrame implements ActionListener {
+    private JTextArea textarea;
+    private String text;
+
+    public Notepad() {
+
+        setTitle("Notepad");
+        ImageIcon logo = new ImageIcon(ClassLoader.getSystemResource("Images/notepad.png"));
+        // Converting ImageIcon to Image Object Becase setIconImage takes only Image
+        // Object
+        Image icon = logo.getImage();
+        setIconImage(icon);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        // setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        // ! Creating the Menu
+        JMenuBar menubar = new JMenuBar();
+        menubar.setBackground(Color.WHITE);
+        // Editing the Menubar
+        // file.setFont(new Font("AERIAL", Font.PLAIN, 14));
+        /*
+         * Creating the File with Options - NEW , OPEN , SAVE, PRINT, EXIT
+         */
+        JMenu file = new JMenu("File");
+        JMenuItem newdoc = new JMenuItem("New File");
+        newdoc.addActionListener(this);
+        newdoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        file.add(newdoc);
+
+        JMenuItem open = new JMenuItem("Open File");
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        open.addActionListener(this);
+        file.add(open);
+
+        JMenuItem save = new JMenuItem("Save File");
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        save.addActionListener(this);
+        file.add(save);
+
+        JMenuItem print = new JMenuItem("Print File");
+        print.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        print.addActionListener(this);
+        file.add(print);
+
+        JMenuItem exit = new JMenuItem("Exit File");
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+        exit.addActionListener(this);
+        file.add(exit);
+
+        // file.add(newdoc);
+
+        menubar.add(file);
+
+        /*
+         * Edit Button
+         * Options - Copy, Cut, Paste, SelectAll
+         */
+        JMenu edit = new JMenu("Edit");
+        JMenuItem copy = new JMenuItem("Copy");
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        copy.addActionListener(this);
+        edit.add(copy);
+
+        JMenuItem cut = new JMenuItem("Cut");
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+        cut.addActionListener(this);
+        edit.add(cut);
+
+        JMenuItem paste = new JMenuItem("Paste");
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        paste.addActionListener(this);
+        edit.add(paste);
+
+        JMenuItem selectall = new JMenuItem("Select All");
+        selectall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        selectall.addActionListener(this);
+        edit.add(selectall);
+
+        menubar.add(edit);
+        /*
+         * About Button
+         * Options - About
+         */
+        JMenu about = new JMenu("About");
+        menubar.add(about);
+        JMenuItem Abt = new JMenuItem("About Developer");
+        Abt.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+        Abt.addActionListener(this);
+        about.add(Abt);
+        setJMenuBar(menubar);
+
+        // !Creating the text area
+        textarea = new JTextArea();
+        textarea.setFont(new Font("SAN_SERIF", Font.PLAIN, 18));
+        textarea.setLineWrap(true);
+        textarea.setWrapStyleWord(true);
+
+        // !Adding Scroll Bar
+        JScrollPane pane = new JScrollPane(textarea);
+
+        add(pane);// ^ScrollBar + TextArea
+
+        setVisible(true);// Should be visivle abfter all other components are rendered
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // ^New File Action
+        if (e.getActionCommand().equals("New File"))
+            textarea.setText("");
+        // ^Open File Action
+        else if (e.getActionCommand().equals("Open File")) {
+            // & Opening File Manager to choose the file
+            JFileChooser chooser = new JFileChooser();
+            chooser.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter restrict = new FileNameExtensionFilter("Only .txt files", "txt");
+            chooser.addChoosableFileFilter(restrict);
+            int action = chooser.showOpenDialog(this);
+
+            // & If No File is choosen , Close the dialogebox and return
+            if (action != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
+            // & Else Read the selected File
+            else {
+                File file = chooser.getSelectedFile();
+                try {
+                    BufferedReader read = new BufferedReader(new FileReader(file));
+
+                    textarea.read(read, null);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        } else if (e.getActionCommand().equals("Save File")) {
+            JFileChooser saveas = new JFileChooser();
+            // Saving the file
+            saveas.setApproveButtonText("Save");
+            int action = saveas.showOpenDialog(this);
+
+            if (action != JFileChooser.APPROVE_OPTION) {
+                return;
+            } else {
+                File filename = new File(saveas.getSelectedFile() + ".txt");
+                BufferedWriter outfile = null;
+                try {
+                    outfile = new BufferedWriter(new FileWriter(filename));
+                    textarea.write(outfile);
+
+                } catch (Exception ae) {
+                    ae.printStackTrace();
+
+                }
+            }
+        } else if (e.getActionCommand().equals("Print File")) {
+            try {
+                textarea.print();
+            } catch (PrinterException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        } else if (e.getActionCommand().equals("Exit File")) {
+            System.exit(0);
+
+        } else if (e.getActionCommand().equals("Copy")) {
+            text = textarea.getSelectedText();
+        } else if (e.getActionCommand().equals("Paste")) {
+            textarea.insert(text, textarea.getCaretPosition());
+        } else if (e.getActionCommand().equals("Cut")) {
+            text = textarea.getSelectedText();
+            textarea.replaceRange("", textarea.getSelectionStart(), textarea.getSelectionEnd());
+        } else if (e.getActionCommand().equals("Select All")) {
+            textarea.selectAll();
+        }
+        else if (e.getActionCommand().equals("About Developer"))
+        {
+            new about().setVisible(true);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Notepad(); // Anonymous Object
+
+    }
+}
---
+++ about.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class about extends JFrame implements ActionListener {
+    JButton b1;
+
+    about() {
+        setBounds(600, 200, 500, 400);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        JLabel l3 = new JLabel(
+            "<html><div style='text-align: center;'>Developer: Sourik Karmakar<br>Contact No: 7810964148<br>Gmail: <a href='mailto:sourikkarmakar2018@gmail.com'>sourikkarmakar2018@gmail.com</a><br>Linkedin: <a href='https://www.linkedin.com/in/sourikkarmakar/'>https://www.linkedin.com/in/sourikkarmakar/</a><br>Github: <a href='https://github.com/thisissourik'>https://github.com/thisissourik</a></div></html>"
+        );
+
+        l3.setFont(new Font("SAN_SERIF", Font.PLAIN, 18));
+        l3.setBounds(50, 40, 400, 200);
+        add(l3);
+
+        b1 = new JButton("OK");
+        b1.setBounds(200, 280, 80, 25);
+        b1.addActionListener(this);
+        add(b1);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == b1) {
+            this.dispose(); // Close the window when "OK" is clicked
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            about about = new about();
+            about.setVisible(true);
+        });
+    }
+}
